d254180f36fae0b314b3c04283931f0f478a47b3
정용재 <yjwlm10@daum.net>
2024-06-01 16:21:58 +0900

    '20240601_Logstash_logger_added'

--- EOF_DESKTOP/utils/Inference/bottle_classifier.py
+++ EOF_DESKTOP/utils/Inference/bottle_classifier.py
@@ -2,7 +2,8 @@
 crop된 영상 프레임에서 target 물체를 classification 하기 위한 모듈입니다.
 """
 from utils.Inference.otx_api import OTXAPI
-
+import socket
+from datetime import datetime
 
 class BottleClassifier(OTXAPI):
     """OTX API를 상속받아 classification을 수행하는 클래스"""
@@ -22,6 +23,8 @@ class BottleClassifier(OTXAPI):
         super().__init__()
         self.current_target = None
         self.set_model_target()
+        self.bottle_logstash_host = '127.0.0.1'
+        self.bottle_logstash_port = 9900
 
     def __str__(self):
         """현재 model이 target하고 있는 물체의 이름을 리턴합니다."""
@@ -55,4 +58,48 @@ class BottleClassifier(OTXAPI):
         return: (target 물체가 유리병인 경우) clear bottle이면 0, lid bottle이면 1
         """
         prediction, _ = self._get_predictions(frame)
+
+        label_or_clear = None
+        pet_or_glass = None
+        prediction_result = int(prediction[-1].get_labels()[-1].get_label().id)
+
+        # variable for logs
+        if prediction_result == 0:
+            label_or_clear = 'CLEAR'
+        else:
+            label_or_clear = 'LABEL'
+
+        if self.current_target == "pet":
+            pet_or_glass = "PET"
+        else:
+            pet_or_glass = "GLASS"
+
+        timestamp = datetime.now().isoformat()
+
+        # create and send log message
+        log_entry = self.create_log_message(timestamp, pet_or_glass, label_or_clear)
+        self.send_log_to_logstash(log_entry)
+
         return int(prediction[-1].get_labels()[-1].get_label().id)
+
+    def send_log_to_logstash(self,log_entry):
+        try:
+            # Logstash TCP 소켓 연결
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((self.bottle_logstash_host, self.bottle_logstash_port))
+
+            # 로그 전송
+            sock.sendall(log_entry.encode('utf-8'))
+            sock.close()
+        except Exception as e:
+            print(f"Error while sending log to Logstash: {e}")
+
+    def create_log_message(self,timestamp,pet_or_glass,label_or_clear):
+        logger_name = pet_or_glass + '_LOG'
+        level = 'INFO'
+        log_message = label_or_clear
+
+        log_entry = f'TIME:{timestamp}  NAME:{logger_name}  LEVEL:{level}  RESULT:{log_message}'
+
+        return log_entry
+        self.send_log_to_logstash(log_entry)
